06e4eaf8b5948fe5d9f19f6bffc399a86d9fa44d
Neos07 <neos07.simo@gmail.com>
2018-12-25 00:12:03 +0100

    Add project files.

---
+++ Playlist maker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playlist_maker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
